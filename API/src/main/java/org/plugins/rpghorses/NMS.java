320a9e47b0563771a881989e0772cdc0b203f557
Rory <roryskipper123@gmail.com>
2024-07-21 13:58:17 -0600

    Implemented 1.21 support

--- API/src/main/java/org/plugins/rpghorses/NMS.java
+++ API/src/main/java/org/plugins/rpghorses/NMS.java
@@ -1,13 +1,24 @@
 package org.plugins.rpghorses;
 
 import org.bukkit.Bukkit;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public abstract class NMS {
 
 	public int errorCount;
 
 	public abstract void removeBehaviour(LivingEntity entity);
+
+	public void removeDurabilityEnchant(ItemStack item) {
+		item.removeEnchantment(Enchantment.DURABILITY);
+		ItemMeta meta = item.getItemMeta();
+		meta.removeItemFlags(ItemFlag.HIDE_ENCHANTS);
+		item.setItemMeta(meta);
+	}
 
 	public void logError(Throwable e) {
 		if (++errorCount <= 10) {
